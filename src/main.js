06d7a7d00420141309d71d0846065e4a0e876765
Fuja <shilevelli@gmail.com>
2023-09-12 21:46:29 +0100

    mmm

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import './assets/main.css'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
