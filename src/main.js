321aa2b2b898dce63775653df567f225ae35e3ce
Fuja <shilevelli@gmail.com>
2023-09-18 03:49:06 +0100

    mhbd

--- src/main.js
+++ src/main.js
@@ -7,12 +7,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// animation on scroll
-
-// import 'aos/dist/aos.css'
-
-// import 'animate.css'
-
 import AOS from 'aos'
 import 'aos/dist/aos.css' // You can also use <link> for styles
 // ..
